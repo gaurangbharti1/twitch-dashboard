05ebcb4bfb6b2b8c60fa207b54baecb0433b084c
Gaurang Bharti <gaurangbharti@gmail.com>
2021-08-18 10:42:31 +0000

    API ready

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,53 @@
 // Main entry point of your app
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import React, { useState } from 'react'
 
 const Home = () => {
+  //State
+  const [favoriteChannels, setFavoriteChannels] = useState([])
+  //Actions
+  const addStreamChannel = async event => {
+    //Prevent the page from redirecting
+    event.preventDefault()
+
+    const { value } = event.target.elements.name
+
+    if(value) {
+      console.log("value: ", value)
+
+      //Call Twitch Search API
+      const path = `https://${window.location.hostname}`
+
+      const response = await fetch(`${path}/api/twitch`, {
+        method: 'POST',
+        headers: {
+          'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({ data : value })
+      })
+
+      const json = await response.json()
+
+      console.log("From the server: ", json.data)
+
+      setFavoriteChannels(prevState => [...prevState, value])
+
+      event.target.elements.name.value = ""
+    }
+    
+    console.log("value: ", value)
+  }
+
+  //Render Methods
+  const renderForm = () => (
+    <div className={styles.formContainer}>
+      <form onSubmit={addStreamChannel}>
+      <input id="name" placeholder="Twitch Channel Name" type="text" required />
+      <button type="submit">Add Streamer</button>
+      </form>
+    </div>
+  )
   return (
     <div className={styles.container}>
       <Head>
@@ -10,7 +55,8 @@ const Home = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className={styles.inputContainer}>
-        <h1>Welcome to the Personalized Twitch Dashboard! 🎉</h1>
+       {renderForm()}
+       <div>{favoriteChannels.join(",")}</div>
       </div>
     </div>
   )
